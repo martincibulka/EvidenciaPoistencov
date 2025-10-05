d4205100a37648633c4418c9f22221c5072d88ac
martin <martin.cibulka@azet.sk>
2025-10-05 19:18:37 +0200

    Pridané nápady.

--- konzolovaAppka/main.py
+++ konzolovaAppka/main.py
@@ -7,9 +7,9 @@ current_page = 0
 vyber_z_menu = 0
 hlasenie = ""
 
-poistenci.append({"meno": "Peter", "priezvisko": "Novák", "vek": 30, "telefon": "0905123456"})
-poistenci.append({"meno": "Jana", "priezvisko": "Kováčová", "vek": 27, "telefon": "0910123456"})
-poistenci.append({"meno": "Dana", "priezvisko": "Kováčová", "vek": 55, "telefon": "0910121234"})
+poistenci.append({"meno": "Peter", "priezvisko": "Novák", "vek": 30, "telefon": "0905123456"})      #   Táto časť
+poistenci.append({"meno": "Jana", "priezvisko": "Kováčová", "vek": 27, "telefon": "0910123456"})    #   slúži len
+poistenci.append({"meno": "Dana", "priezvisko": "Kováčová", "vek": 55, "telefon": "0910121234"})    #   na testovanie
 
 while not exit:
     hlavicka(current_page, hlasenie)
